537a5ec7066fcf96f84d325b6357ed9cbbf0f904
Bodemola123 <112621700+Bodemola123@users.noreply.github.com>
2025-09-05 08:09:29 +0100

    Changed from isProduction to true and sameSite, None

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ app.use(
     }),
     cookie: {
       httpOnly: true,                    // prevents JS access to cookie
-      secure: isProduction,              // ✅ HTTPS required in production
-      sameSite: isProduction ? "none" : "lax" , // cross-site cookies only in prod
+      secure: true,              // ✅ HTTPS required in production
+      sameSite: "none"  , // cross-site cookies only in prod
       maxAge: 7 * 24 * 60 * 60 * 1000,   // 7 days
     },
   })
