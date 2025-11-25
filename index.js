3bcf8511609db6b9d15b62dffad418a80469a9f3
Bodemola <bodemola123@gmail.com>
2025-11-25 08:41:43 +0100

    changed to service "gmail"

--- index.js
+++ index.js
@@ -83,9 +83,7 @@ const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || process.env.JWT_SECRET 
 
 // Nodemailer transporter
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // use STARTTLS
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
